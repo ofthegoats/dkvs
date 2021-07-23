da9999b6a3087a3d982c17692da3862b294d0383
piokozi <47647338+PioKozi@users.noreply.github.com>
2021-07-23 16:04:31 +0100

    refactor(node): small functions into node_common.go

--- node.go
+++ node.go
@@ -5,7 +5,6 @@ import (
 	"encoding/gob"
 	"log"
 	"math"
-	"math/rand"
 	"sync"
 	"time"
 
@@ -115,26 +114,4 @@ func (N *Node) Gossip() error {
 			N.SendNextRound(msg)
 		}
 	}
-}
-
-func (N *Node) UpdateData(key, newvalue string) {
-	oldValue := N.Data[key]
-	N.Data[key] = newvalue // Change the existing value, or make a new one
-	log.Printf("%s :Updated \"%s\" FROM \"%s\" TO \"%s\"", N.socket, key, oldValue, newvalue)
-}
-
-func (N *Node) SendNextRound(msg Rumour) {
-	if msg.T <= N.MaxRounds {
-		msg.T++ // increment the round on the message by one
-		for i := 0; i < N.b; i++ {
-			neighbour := N.GetRandomNeighbour()
-			log.Printf("%s is sending to %s", N.socket, neighbour)
-			N.Send(neighbour, msg)
-		}
-	}
-}
-
-func (N *Node) GetRandomNeighbour() string {
-	i := rand.Intn(len(N.Neighbours))
-	return N.Neighbours[i]
 }
---
+++ node_common.go
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////
+//  This file should contain the smaller methods of Node  //
+////////////////////////////////////////////////////////////
+
+package main
+
+import (
+	"log"
+	"math/rand"
+)
+
+func (N *Node) UpdateData(key, newvalue string) {
+	oldValue := N.Data[key]
+	N.Data[key] = newvalue // Change the existing value, or make a new one
+	log.Printf("%s :Updated \"%s\" FROM \"%s\" TO \"%s\"", N.socket, key, oldValue, newvalue)
+}
+
+func (N *Node) SendNextRound(msg Rumour) {
+	if msg.T <= N.MaxRounds {
+		msg.T++ // increment the round on the message by one
+		for i := 0; i < N.b; i++ {
+			neighbour := N.GetRandomNeighbour()
+			log.Printf("%s is sending to %s", N.socket, neighbour)
+			N.Send(neighbour, msg)
+		}
+	}
+}
+
+func (N *Node) GetRandomNeighbour() string {
+	i := rand.Intn(len(N.Neighbours))
+	return N.Neighbours[i]
+}
