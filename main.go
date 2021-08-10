fb87491fc0687cd644aaa38f62c34d7e00fc72ab
piokozi <47647338+PioKozi@users.noreply.github.com>
2021-08-10 13:46:37 +0100

    refactor(main.go): changed definitions in main.go
    main.go should now be easier to work with
    Also updated to test FSCs, however this will be removed in future and can be ignored.

--- main.go
+++ main.go
@@ -6,32 +6,35 @@ import (
 )
 
 func main() {
-	n := NewNode([]string{"tcp://127.0.0.1:9999", "tcp://localhost:9996"}, "tcp://127.0.0.1:9998", 1*time.Second, 2*time.Second, 2, 10)
+	tcptimeout := 1 * time.Second
+	rttperiod := 60 * time.Second
+	fscperiod := 2 * time.Second // low, for testing
+	n := NewNode(
+		[]string{}, "tcp://localhost:9999",
+		tcptimeout, rttperiod, fscperiod,
+		5, 2,
+	)
 	go n.Gossip()
-	time.Sleep(1 * time.Second)
-
-    m := NewNode([]string{"tcp://127.0.0.1:9998", "tcp://localhost:9996"}, "tcp://127.0.0.1:9999", 1*time.Second, 2*time.Second, 2, 10)
-	go m.Gossip()
-	time.Sleep(1 * time.Second)
-
-	log.Printf("%s: maxrounds = %d\n", n.socket, n.MaxRounds)
-	log.Printf("%s: maxrounds = %d\n", m.socket, m.MaxRounds)
-
-	r1 := Rumour{
+	time.Sleep(2 * time.Second) // time to catch up
+	r := Rumour{
 		RequestType: UpdateData,
-		Key:         "key1",
-		NewValue:    "val1",
-		T:           0,
+		Key:         "newkey",
+		NewValue:    "newvalue",
 	}
-	n.Send("tcp://127.0.0.1:9998", r1) // socket DOES exist
-	n.Send("tcp://127.0.0.1:9997", r1) // socket does NOT exist
-	n.Send("tcp://127.0.0.1:9996", r1) // socket does NOT exist
+	go n.Send("tcp://localhost:9999", r)
 
-	time.Sleep(1 * time.Second) // give time to catch up
+	// m spawns knowing about n
+	m := NewNode(
+		[]string{"tcp://localhost:9999"}, "tcp://localhost:9998",
+		tcptimeout, rttperiod, fscperiod,
+		5, 2,
+	)
+	go m.Gossip()
+	time.Sleep(2 * time.Second) // time to catch up
 
 	for {
 		log.Printf("%s: %v, %v\n", n.socket, n.Data, n.Neighbours)
-		log.Printf("%s: %v, %v\n", m.socket, m.Data, n.Neighbours)
+		log.Printf("%s: %v, %v\n", m.socket, m.Data, m.Neighbours)
 		time.Sleep(5 * time.Second)
 	}
 }
