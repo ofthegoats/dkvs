472c2f8bbc255304e18a747c896f2c373d6d1c21
piokozi <47647338+ofthegoats@users.noreply.github.com>
2021-08-31 12:57:27 +0100

    refactor(nodes): removed second node from main.go
    Moving on, there should now only be one node there.

--- main.go
+++ main.go
@@ -20,27 +20,8 @@ func main() {
 		9999, 5, 2,
 	)
 	go n.Gossip()
-	// m spawns knowing about n
-	m := NewNode(
-		[]string{"tcp://localhost:9999"}, "localhost",
-		key,
-		tcptimeout, rttperiod, fscperiod,
-		9998, 5, 2,
-	)
-	go m.Gossip()
-
-    time.Sleep(5 * time.Second)
-
-	r := Rumour{
-		RequestType: UpdateData,
-		Key:         "newkey",
-		NewValue:    "newvalue",
-	}
-	go m.Send("tcp://localhost:9998", r)
-
 	for {
         log.Printf("%s:%d: %v, %v\n", n.LANIP, n.Port, n.Data, n.Neighbours)
-        log.Printf("%s:%d: %v, %v\n", m.LANIP, m.Port, m.Data, m.Neighbours)
 		time.Sleep(5 * time.Second)
 	}
 }
