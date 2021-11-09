6b59929d87ca4b8f4fbdc7f8857848ec52792002
ofthegoats <47647338+ofthegoats@users.noreply.github.com>
2021-11-09 10:42:25 +0000

    feat(list-values): implemented basic list-values

--- dkvs-front/main.go
+++ dkvs-front/main.go
@@ -1,11 +1,65 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"os"
+	"time"
 
-	. "github.com/ofthegoats/dkvs/dkvs-back"
+	. "github.com/ofthegoats/dkvs/dkvs-back/node"
+	. "github.com/ofthegoats/dkvs/dkvs-back/rumour"
 )
 
+// syntax: dkvs-front $PORT $KEY $COMMAND [ $KEY | $KEY $VALUE | $SOCKET ]
+
+func checkArgs() error {
+	err := errors.New("not enough arguments")
+	length := len(os.Args)
+	if length < 3 { // always necessary: port, key, command
+		return err
+	}
+	switch {
+	case os.Args[3] == "add-neighbour" && length < 4:
+		return err
+	case os.Args[3] == "remove-neighbour" && length < 4:
+		return err
+	case os.Args[3] == "get-value" && length < 4:
+		return err
+	case os.Args[3] == "set-value" && length < 5:
+		return err
+	}
+	return nil
+}
+
 func main() {
-	fmt.Println("placeholder")
+	// check if the correct amount of arguments has been passed
+	if err := checkArgs(); err != nil {
+		log.Fatalln(err)
+	}
+	port := os.Args[1] // port on which the *backend* is listening
+	key := []byte(os.Args[2])
+	command := os.Args[3]
+	// fairly arbitrary values taken from back/main.go
+	// TODO: replace with unix sockets
+	tcptimeout := 1 * time.Second
+	rttperiod := 300 * time.Second
+	fscperiod := 300 * time.Second
+	n := NewNode(
+		[]string{}, "localhost",
+		key,
+		tcptimeout, rttperiod, fscperiod,
+		9875, 5, 2,
+	)
+	messages := make(chan Rumour)
+	listener := fmt.Sprintf("tcp://localhost:%d", 9875)
+	go n.Listen(listener, messages)
+	switch command {
+	default:
+		log.Fatalf("not a valid command: %s\n", command)
+	case "list-values":
+		n.RequestCopy(fmt.Sprintf("tcp://localhost:%s", port))
+		fsc := <-messages
+		fmt.Printf("%v\n", fsc)
+	}
 }
